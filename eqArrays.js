8cfc5e0732102ba4dd0e64c98292e998f8b5966e
Ehab Ahmed <ehabahmed2456@gmail.com>
2023-10-18 22:36:16 -0400

    without function added and modded eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -9,6 +9,9 @@ const assertEqual = function(actual, expected) {
 //  Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match. 
 
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false
+  }
   for (let i = 0; i < arr1.length; i++) {
     if(arr1[i] !== arr2[i]){
       return false;
@@ -19,8 +22,8 @@ const eqArrays = function(arr1, arr2) {
 
 //assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
-//console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-//console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays([1, 2, 3, 4], [1, 2, 3, 4])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
 //console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 //console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
--- without.js
+++ without.js
@@ -1,4 +1,7 @@
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false
+  }
   for (let i = 0; i < arr1.length; i++) {
     if(arr1[i] !== arr2[i]){
       return false;
@@ -15,4 +18,20 @@ const assertArraysEqual = function(arry1, arry2) {
       console.log("❌❌❌ The 2 arrays are equal", result)
     }
 }
+// implement without which will return a subset of a given array, removing unwanted elements.
+// This function should take in a source array and a itemsToRemove array. 
+// It should return a new array with only those elements from source that are not present in the itemsToRemove array.
+// Here are a couple examples, but please don't limit your testing to just these scenarios:
 
+function without(source, itemsToRemove) {
+  const result = [];
+  for (const element of source) {
+    if (!itemsToRemove.includes(element)) {
+      result.push(element);
+    }
+  }
+
+  return result;
+}
+console.log(without([1, 2, "apple",  3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2
