23837e2202914e62609cb4037d4f0ea0625234af
Ehab Ahmed <ehabahmed2456@gmail.com>
2023-10-18 21:20:21 -0400

    created assertArraysEqual Function

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+
+const assertArraysEqual = function(arry1, arry2) {
+  let result = eqArrays(arry1, arry2)
+    if(result){
+      console.log("✅✅✅ The 2 arrays are equal:", result)
+    } else{
+      console.log("❌❌❌ The 2 arrays are equal", result)
+    }
+}
+//assertArraysEqual([1, 2, 3], [1, 2, 3])// => true
+//assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
---
+++ without.js
@@ -0,0 +1,18 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(arry1, arry2) {
+  let result = eqArrays(arry1, arry2)
+    if(result){
+      console.log("✅✅✅ The 2 arrays are equal:", result)
+    } else{
+      console.log("❌❌❌ The 2 arrays are equal", result)
+    }
+}
+
