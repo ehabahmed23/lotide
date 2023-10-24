38bcc753ee156f3a4bbcb38fcca85c7c4e9435fe
Ehab Ahmed <ehabahmed2456@gmail.com>
2023-10-23 23:58:09 -0400

    updated headTest file with test codes

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,14 +1,6 @@
 const assertEqual = require('../assertEqual');
-const head = require('../head');
 
 //TEST CODE//
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
-//TEST CODE//
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-//TEST CODE//
-//TEST CODE//
-//TEST CODE//
-//TEST CODE//
-//TEST CODE//
+
