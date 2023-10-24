592aad1d12ceeb8be68a3f87a665c39a3777292d
Ehab Ahmed <ehabahmed2456@gmail.com>
2023-10-24 16:07:35 -0400

    more mocha and chai mods

--- test/middleTest.js
+++ test/middleTest.js
@@ -4,9 +4,9 @@ const assert = require('chai').assert;
 
 describe("#middle Test", () => {
   it("returns true if [2, 3,] are the middle indexes of [1, 2, 3, 4] ", () => {
-    assert.strictEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true);
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
   it("returns false if [2, 4] are not the middle indexes of [1, 2, 3, 4, 5, 6]", () => {
-    assert.strictEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [2, 4]), false); 
+    assert.notDeepEqual(middle([1, 2, 3, 4, 5, 6]), [2, 4]); 
   });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,12 @@
-const eqArrays = require('../eqArrays');
 const tail = require('../tail');
 const assert = require('chai').assert;
 
 
 describe("#tail Test", () => {
   it('returns true if the tail of this ["Yo Yo", "Lighthouse", "Labs"] === ["Lighthouse", "Labs"]', () => {
-    assert.strictEqual(eqArrays(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]), true);
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"],);
   });
   it('returns true if the tail of this [8, 9, 10, 11] === [9, 10, 11]', () => {
-    assert.strictEqual(eqArrays(tail([8, 9, 10, 11]), [9, 10, 11]), true);
+    assert.deepEqual(tail([8, 9, 10, 11]), [9, 10, 11]);
   });
 });
